a799d144e9d05aa9f40b3c6a8afb3b487d35e171
Rakha' Adrida Bagaspati <62930338+rakhaadrida@users.noreply.github.com>
2026-07-18 02:07:05 +0700

    [feat] Dashboard Page

--- src/controllers/admin/dashboard.controller.js
+++ src/controllers/admin/dashboard.controller.js
@@ -1,7 +1,34 @@
+const Customer = require("../../models/customer.model");
+const Booking = require("../../models/booking.model");
+const Property = require("../../models/property.model");
+
 module.exports = {
-    dashboard: (req, res) => {
-        res.render("admin/dashboard/dashboard", {
-            title: "Isakha Rentals | Dashboard",
-        });
+    dashboard: async (req, res) => {
+        try {
+            const customer = await Customer.find();
+            const booking = await Booking.find();
+            const property = await Property.find();
+
+            const earnings = await Booking.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalEarning: { $sum: "$total" },
+                    },
+                },
+            ]);
+
+            const totalEarnings = earnings[0]?.totalEarning || 0;
+
+            res.render("admin/dashboard/dashboard", {
+                title: "Isakha Rentals | Dashboard",
+                customer,
+                booking,
+                property,
+                totalEarnings,
+            });
+        } catch (error) {
+            res.redirect("/admin/banks");
+        }
     },
 };
