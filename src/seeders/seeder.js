3ccb5b72c2f4694e28a7a3046c42b5afd2de8684
Rakha' Adrida Bagaspati <62930338+rakhaadrida@users.noreply.github.com>
2026-07-22 15:43:56 +0700

    [fix] seeder

--- src/seeders/seeder.js
+++ src/seeders/seeder.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const path = require("path");
 const bcrypt = require("bcryptjs");
@@ -6,8 +8,7 @@ const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const modelsPath = path.join(__dirname, "..", "models");
-const uri =
-    "mongodb+srv://rakhaab_db_user:godenzonen@cluster0.1n4kexr.mongodb.net/property_rental?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 const modelMap = {
     Booking: require(path.join(modelsPath, "booking.model.js")),
